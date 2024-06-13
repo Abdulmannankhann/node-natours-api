95fe332734e639a564e149c16095afb0f213a689
Abdul9.khan <Abdul9.khan@ril.com>
2024-06-14 01:41:04 +0530

    Add delete current user api, Implement best security practices

--- app.js
+++ app.js
@@ -1,23 +1,69 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
+
 const tourRouter = require('./routes/toursRoutes');
 const userRouter = require('./routes/usersRoutes');
-const AppError = require("./utils/appError")
-const globalErrorHandler = require("./controllers/errorController")
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 
 const app = express();
 
-//1) MIDDLEWARES
+//Set Security HTTP headers
+app.use(helmet());
+
+//1) Development Logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-app.use(express.json());
+//Limit request from same API
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many request from this IP, Please try again in an hour',
+});
+
+app.use('/api', limiter);
+
+//Body parser, reading data from body into req.body
+app.use(
+  express.json({
+    limit: '10kb',
+  }),
+);
+
+//Data Sanitization against NoSQL query Injection
+app.use(mongoSanitize());
+
+//Data sanitization against XSS
+app.use(xss());
+
+//Prevent Parameter Pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  }),
+);
+
+//Serving static files
 app.use(express.static(`${__dirname}/public`));
 
+//Test Middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
-  console.log(req.headers)
+  console.log(req.headers);
   next();
 });
 
@@ -25,8 +71,9 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
+//Global Error Handler
 app.all('*', (req, res, next) => {
-  next(new AppError(`Can't find ${req.originalUrl} on this server.`,404));
+  next(new AppError(`Can't find ${req.originalUrl} on this server.`, 404));
 });
 
 app.use(globalErrorHandler);
